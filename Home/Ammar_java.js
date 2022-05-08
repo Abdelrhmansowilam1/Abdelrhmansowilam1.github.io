35f4dbb9a442ef74df7ebb51a4a76d7d3c30d158
Abdelrhmansowilam1 <103390923+Abdelrhmansowilam1@users.noreply.github.com>
2022-05-08 21:40:29 +0200

    Add files via upload

--- Home/Ammar_java.js
+++ Home/Ammar_java.js
@@ -1,5 +1,14 @@
+
 function switch_theme(){
     var element1=document.body;
     element1.classList.toggle("switch");
 }
-    
+function show(){
+    var change=document.getElementById("");
+    change.onfocus =function(){
+        document.getElementById("").style.display="block";
+    }
+    change.onblur =function(){
+        document.getElementById("").style.display="none";
+    }
+}
