44f70e1f2f8d780dd14eebe04885036f8fad4aa3
Abdelrhmansowilam1 <103390923+Abdelrhmansowilam1@users.noreply.github.com>
2022-05-10 08:31:52 +0200

    Add files via upload

---
+++ omar Tamer/marvel/button.js
@@ -0,0 +1,13 @@
+var btnTop = document.getElementById("btntop")
+
+
+btnTop.addEventListener("click",function(){
+
+
+    window.scroll({
+       top:0,
+       left:0,
+       behavior: 'smooth'
+
+    });
+});
